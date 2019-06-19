de7d8a8347f70b6700a4a4f8ee7cdebe3de81fc4
Gimel <hao.wang2@tendcloud.com>
2019-06-19 15:30:40 +0800

    change rscv to hyperopt

--- mlstacking/report.py
+++ mlstacking/report.py
@@ -2,11 +2,13 @@ import base64
 import io
 import os
 import warnings
+from functools import partial
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pydotplus
+from hyperopt import STATUS_OK, Trials, fmin, hp, space_eval, tpe
 from sklearn import tree
 from sklearn.base import clone
 from sklearn.ensemble import RandomForestClassifier
@@ -26,29 +28,29 @@ class GenerateReport():
             'XG': XGBClassifier(n_jobs=-1),
             'DT': tree.DecisionTreeClassifier(max_depth=3),
         }
-        self.param_distributions = {}
-        self.param_distributions['XG'] = {
-            'max_depth': [3, 5, 7, 9, 11],
-            'learning_rate': [0.01, 0.05, 0.1, 0.15, 0.2],
-            'n_estimators': [50, 100, 150, 200, 250],
-            'min_child_weight': [0, 0.1, 1, 5, 10],
-            'max_delta_step': [0, 0.1, 1, 5, 10],
-            'subsample': [1, 0.9, 0.8],
-            'colsample_bylevel': [1, 0.9, 0.8],
-            'reg_alpha': [0.1, 1, 3, 5, 10],
-            'reg_lambda': [0.1, 1, 3, 5, 10],
+        self.space = {}
+        self.space['XG'] = {
+            'max_depth': hp.choice('max_depth', [*range(1,16)]),
+            'learning_rate': hp.loguniform('learning_rate', np.log(0.01), np.log(0.5)),
+            'n_estimators': hp.choice('n_estimators', [*range(25,275,25)]),
+            'min_child_weight': hp.uniform("min_child_weight", 0, 10),
+            'max_delta_step': hp.uniform("max_delta_step", 0, 10),
+            'subsample': hp.uniform('subsample', 0.8, 1),
+            'colsample_bylevel': hp.uniform('colsample_bylevel', 0.8, 1),
+            'reg_alpha':  hp.uniform("reg_alpha", 0, 10),
+            'reg_lambda': hp.uniform("reg_lambda", 0, 10),
         }
-        self.param_distributions['LR'] = {
-            'C': [0.1,0.5,1,3,5],
-            'solver': ['newton-cg', 'lbfgs', 'liblinear','sag', 'saga'],
+        self.space['LR'] = {
+            'C': hp.uniform("C", 0, 10),
+            'solver': hp.choice('solver', ['newton-cg', 'lbfgs', 'liblinear','sag', 'saga']),
         }
-        self.param_distributions['RF'] = {
-            'max_depth': [3, 5, 7, 9, 11], 
-            'n_estimators': [50, 100, 150, 200, 250],
-            'criterion': ['gini','entropy'],
-            'min_samples_split': [3, 5, 7, 9, 11], 
-            'min_samples_leaf': [3, 5, 7, 9, 11], 
-            'max_features': ['auto', 'log2', 0.5],
+        self.space['RF'] = {
+            'max_depth': hp.choice('max_depth', [*range(1,16)]),
+            'n_estimators': hp.choice('n_estimators', [*range(25,275,25)]),
+            'criterion': hp.choice('criterion', ['gini','entropy']),
+            'min_samples_split': hp.uniform('min_samples_split', 0, 100), 
+            'min_samples_leaf': hp.uniform('min_samples_leaf', 0, 100), 
+            'max_features': hp.choice('max_features', ['auto', 'log2', 0.5]),
         }
         self.classifiers = classifiers
         self.search = search
@@ -67,13 +69,15 @@ class GenerateReport():
                 'Score': cross_val_score(instance, features, labels, cv=4, scoring='roc_auc').mean()
             })
             if self.search:
-                rscv = RandomizedSearchCV(estimator=instance, param_distributions=self.param_distributions[classifier], n_iter=20, scoring='roc_auc', cv=4)
-                rscv.fit(features,labels)
-                instance.set_params(**rscv.best_params_)
+                objective = partial(self.objective, model=instance, features=features,labels=labels)
+                trials = Trials()
+                max_evals = self.search if isinstance(self.search, int) else 10
+                best = fmin(fn=objective, space=self.space[classifier], trials=trials, algo=tpe.suggest, max_evals=max_evals)
+                instance.set_params(**space_eval(self.space[classifier], best))
                 self.results.append({
                     'Name': classifier+'_Tuned',
                     'Params': instance.get_params(),
-                    'Score': rscv.best_score_
+                    'Score': -trials.best_trial['result']['loss'],
                 })
             instance.fit(X_train, y_train)
             predict = [i[1] for i in instance.predict_proba(X_test)]
@@ -81,6 +85,7 @@ class GenerateReport():
             precision, recall, _ = precision_recall_curve(y_test, predict)
 
             plt.subplot(len(self.classifiers),2,i).plot(fpr, tpr)
+            plt.plot([0,1],[0,1],ls='--')
             plt.title(classifier+' Receiver Operating Characteristic Curve')
             plt.xlabel('False Positive Rate')
             plt.ylabel('True Positive Rate')
@@ -106,8 +111,6 @@ class GenerateReport():
         plt.savefig(self.imgbuffer,format='png')
 
     def print(self,filepath):
-        
-        # 
         if os.path.isfile(filepath):
             if os.path.exists(filepath):
                 os.remove(filepath)
@@ -131,3 +134,7 @@ class GenerateReport():
                 form_out = io.StringIO()
                 feature_importance_out.to_csv(form_out, sep="|", index=False)
                 file.write(form_out.getvalue()) 
+        
+    def objective(self, hyperparams, model, features, labels):
+        score = cross_val_score(model, features, labels, cv=4, scoring='roc_auc').mean()
+        return {'loss': -score, 'status': STATUS_OK}
